c9cb9c407d1305ccf89a39961db3eb9706bf124a
SHDWI <shdwi@orsted.dk>
2025-05-17 23:10:45 +0800

    code changes

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,7 +5,7 @@ from io import StringIO
 def test_say_hello():
     with patch('sys.stdout', new=StringIO()) as fake_out:
         say_hello()
-        assert fake_out.getvalue().strip() == "Hello, World!"
+        assert fake_out.getvalue().strip() == "Hello, Universe!"
 
 def test_fail_say_hello():
     with patch('sys.stdout', new=StringIO()) as fake_out:
