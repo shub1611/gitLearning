89dfff63fa2e68dab471b66b4d2395d96ef90398
Shubham Dwivedi <shdwi@orsted.com>
2025-05-22 12:38:45 +0800

    add if statement

--- tests/test_env_app.py
+++ tests/test_env_app.py
@@ -2,7 +2,7 @@ from env_app import get_app_env
 
 def test_get_app_env():
     app_env = get_app_env()
-    assert app_env == 'test'
+    assert app_env == 'testing'
 
 def test_get_app_env_no_env(monkeypatch):
     monkeypatch.delenv('APP_ENV', raising=False)
