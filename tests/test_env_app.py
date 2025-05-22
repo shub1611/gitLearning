22ea31062c5542e95d7bf5e787d83a96ad6d4c47
Shubham Dwivedi <shdwi@orsted.com>
2025-05-22 13:36:17 +0800

    test fix

--- tests/test_env_app.py
+++ tests/test_env_app.py
@@ -2,7 +2,7 @@ from env_app import get_app_env
 
 def test_get_app_env():
     app_env = get_app_env()
-    assert app_env == 'testing'
+    assert app_env == 'test'
 
 def test_get_app_env_no_env(monkeypatch):
     monkeypatch.delenv('APP_ENV', raising=False)
